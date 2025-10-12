fa960b4ab51825df240c75661fe54ad9155b11e9
Orit Gegelia <o.gegelia@yandex.ru>
2025-10-12 22:47:42 +0300

    the path for logs changed

--- test-script.bash
+++ test-script.bash
@@ -2,6 +2,6 @@
 count=0
 
 while true; do
-echo "Test-script running. $(date +%Y-%m-%d\ %H:%M:%S): `curl -s x.x.x.x/sensor` Output $((count++))" > test-log.txt
+echo "Test-script running. $(date +%Y-%m-%d\ %H:%M:%S): $(curl -s x.x.x.x/sensor) Output $((count++))" > /var/log/test-script.log
 sleep 5
 done
