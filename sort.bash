d6c1a82556b9792b7f808748a3cdb7eaa5589b18
Orit Gegelia <o.gegelia@yandex.ru>
2025-10-13 15:26:27 +0300

    фикс для стягивания-4

--- sort.bash
+++ sort.bash
@@ -3,6 +3,7 @@
 exec > ~/log/sort.log 2>&1
 count=0
 while true; do
+echo '' > ~/log/sort.log
 echo "Script running. Output $((count++)) $(date +%Y-%m-%d\ %H:%M:%S): $(curl -s x.x.x.x/sensor)"
 ps -eo ppid,pid,%mem,command | awk -v mem_kb="$(grep MemTotal /proc/meminfo | awk '{print $2}')" '{mem[$1]+=$3; cmd[$1]=$4} END {for (NAME in mem) print NAME, cmd[NAME], int(mem[NAME]*mem_kb/100/1024)" MiB"}' OFS=',' | sort -t',' -k3,3rn
 sleep 5
