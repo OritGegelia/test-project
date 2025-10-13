96642af4dbafbdc8796075637882d0c109bfdec1
Orit Gegelia <o.gegelia@yandex.ru>
2025-10-13 14:42:33 +0300

    пробуем очистить файл так-2

--- sort.bash
+++ sort.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Sort processes by memory usage and log output to /var/log/sort.log
+: > /var/log/sort.log
 exec > /var/log/sort.log 2>&1
 count=0
 while true; do
-echo '' > /var/log/sort.log
 echo "Script running. Output $((count++)) $(date +%Y-%m-%d\ %H:%M:%S): $(curl -s x.x.x.x/sensor)"
 ps -eo ppid,pid,%mem,command | awk -v mem_kb="$(grep MemTotal /proc/meminfo | awk '{print $2}')" '{mem[$1]+=$3; cmd[$1]=$4} END {for (NAME in mem) print NAME, cmd[NAME], int(mem[NAME]*mem_kb/100/1024)" MiB"}' OFS=',' | sort -t',' -k3,3rn
 sleep 5
