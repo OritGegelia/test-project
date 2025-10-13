fd02492580debfb5ffa00b90660a2f87ca8f80d0
Orit Gegelia <o.gegelia@yandex.ru>
2025-10-13 14:08:58 +0300

    УПОРСТВО

--- sort.bash
+++ sort.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Sort processes by memory usage and log output to /var/log/sort.log
+: > /var/log/sort.log
+exec > /var/log/sort.log 2>&1
 count=0
 while true; do
 echo "Script running. Output $((count++)) $(date +%Y-%m-%d\ %H:%M:%S): $(curl -s x.x.x.x/sensor)"
