2a9769b4a886c1f731a490a285484c3265173eb6
Orit Gegelia <o.gegelia@yandex.ru>
2025-10-13 15:42:11 +0300

    пробуем вообще без exec

--- sort.bash
+++ sort.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
-: > ~/log/sort.log
-exec > ~/log/sort.log 2>&1
-while true; do
-echo '' > ~/log/sort.log
-echo "Script running. $(date +%Y-%m-%d\ %H:%M:%S): $(curl -s x.x.x.x/sensor)"
-ps -eo ppid,pid,%mem,command | awk -v mem_kb="$(grep MemTotal /proc/meminfo | awk '{print $2}')" '{mem[$1]+=$3; cmd[$1]=$4} END {for (NAME in mem) print NAME, cmd[NAME], int(mem[NAME]*mem_kb/100/1024)" MiB"}' OFS=',' | sort -t',' -k3,3rn
+while true; do {
+  echo '' > 
+  echo "Script running. Output $((count++)) $(date +%Y-%m-%d\ %H:%M:%S): $(curl -s x.x.x.x/sensor)"
+  ps -eo ppid,pid,%mem,command | awk -v mem_kb="$(grep MemTotal /proc/meminfo | awk '{print $2}')" '{mem[$1]+=$3; cmd[$1]=$4} END {for (NAME in mem) print NAME, cmd[NAME], int(mem[NAME]*mem_kb/100/1024)" MiB"}' OFS=',' | sort -t',' -k3,3rn
+} > ~/log/sort.log 2>&1 
 sleep 5
 done
