3e7f73f63310abad0204325008361ef1142971c1
Orit Gegelia <o.gegelia@yandex.ru>
2025-10-14 19:59:44 +0300

    замена пути

--- sort.bash
+++ sort.bash
@@ -3,6 +3,6 @@ count=0
 while true; do {
   echo "Script running. Output $((count++)) $(date +%Y-%m-%d\ %H:%M:%S): $(curl -s x.x.x.x/sensor)"
   ps -eo ppid,pid,%mem,command | awk -v mem_kb="$(grep MemTotal /proc/meminfo | awk '{print $2}')" '{mem[$1]+=$3; cmd[$1]=$4} END {for (NAME in mem) print NAME, cmd[NAME], int(mem[NAME]*mem_kb/100/1024)" MiB"}' OFS=',' | sort -t',' -k3,3rn
-} > ~/log/sort.log 2>&1 
+} > /var/log/sort.log 2>&1 
 sleep 5
 done
