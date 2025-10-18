fa2ae4c355161950a2ee9f7b5c7d475fc4768317
Orit Gegelia <o.gegelia@yandex.ru>
2025-10-18 16:45:40 +0300

    for fix - without while

--- new-sort.bash
+++ new-sort.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 count=0
-while true; do {
   #строка номера вывода и текущей даты
   echo "Script running. Output $((count++)) $(date +%Y-%m %d\ %H:%M:%S): $(curl -s x.x.x.x/sensor)"
 
@@ -8,8 +7,3 @@ while true; do {
   sort_result=$(ps -eo ppid,pid,%mem,cmd --no-headers | python3 n-sort.py)
 
   echo "$sort_result"
-  
-
-} > /var/log/new-sort.log 2>&1 
-sleep 5
-done
