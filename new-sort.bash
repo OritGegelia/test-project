7cff5031c765b4709fcd016217f246b42a183465
Orit Gegelia <o.gegelia@yandex.ru>
2025-10-18 19:18:39 +0300

    ошибочка в имени файла

--- new-sort.bash
+++ new-sort.bash
@@ -20,11 +20,11 @@ n=500
     echo "Total system memory: $(free -h | grep Mem | awk '{print $2}')"
     echo "System memory usage: $(free -h | grep Mem | awk '{print $3 "/" $2}')"
     echo "Status: $status"
-    
+
     # чтение данных о процессах, построение дерева процессов и вызов функции обхода
     sort_result=$(ps -eo ppid,pid,%mem,cmd --no-headers | python3 n-sort.py)
     echo "PPID, MEMORY, COMMAND"
     echo "$sort_result"
-  } > ~/var/log/neww-sort.log
+  } > ~/var/log/new-sort.log
   sleep 5  
   done
