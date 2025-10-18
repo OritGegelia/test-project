74c5744a54968f3c267379839c133adbb4ab0bba
Orit Gegelia <o.gegelia@yandex.ru>
2025-10-18 16:51:20 +0300

    date fix

--- new-sort.bash
+++ new-sort.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 count=0
   #строка номера вывода и текущей даты
-  echo "Script running. Output $((count++)) $(date +%Y-%m %d\ %H:%M:%S): $(curl -s x.x.x.x/sensor)"
+  echo "Script running. Output $((count++)) $(date '+%Y-%m-%d %H:%M:%S'): $(curl -s x.x.x.x/sensor)"
 
   #чтение данных о процессах, построение дерева процессов и вызов функции обхода
   sort_result=$(ps -eo ppid,pid,%mem,cmd --no-headers | python3 n-sort.py)
